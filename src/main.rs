9eeff0bc5065f2995726754382296318d4aedcc1
Sacha Muszlak <sacha@muszlak.com>
2023-05-05 17:33:04 +0200

    Added iced as a dependency

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,49 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+/// state of the app
+struct Counter {
+    // The counter value
+    value: i32,
+}
+
+/// User interactions 
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+use iced::widget::{button, column, text, Column};
+
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `IncrementPressed` message when pressed
+            button("+").on_press(Message::IncrementPressed),
+
+            // We show the value of the counter here
+            text(self.value).size(50),
+
+            // The decrement button. We tell it to produce a
+            // `DecrementPressed` message when pressed
+            button("-").on_press(Message::DecrementPressed),
+        ]
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+}
 
 fn main() -> Result<()> {
     env_logger::init(); // loging 
